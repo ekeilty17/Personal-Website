a2c5e6775e75088ff85c28b203fea37038532032
Standard <Standard@inside-65-95-24.wireless.utoronto.ca>
2018-09-17 17:21:34 -0400

    Resume Section first Draft

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as jsPDF from 'jspdf';
+import * as html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-resume',
@@ -11,4 +13,21 @@ export class ResumeComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  // TODO: This is implemented poorly
+  public downloadPDF(quality = 1) {
+    
+    const filename  = 'Resume_EricKeilty';
+
+		html2canvas(document.getElementById('resume'), 
+								{scale: quality}
+						 ).then(canvas => {
+			let pdf = new jsPDF('p', 'mm', 'letter');
+			pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
+			pdf.save(filename + '.pdf');
+		});
+    
+
+  }
+
 }
